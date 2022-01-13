ec9ea6dc2169c6c1bc391d877a4a3f802f57a613
jonekell1002 <jonekell1002@gmail.com>
2022-01-13 15:52:33 +0100

    Modify Testing script

--- test/LinearVesting.test.js
+++ test/LinearVesting.test.js
@@ -1,11 +1,12 @@
-const { expect, assert } = require("chai");
-const { BigNumber } = require("ethers");
-const { ethers } = require("hardhat");
+const { expect, assert } = require('chai');
+const { BigNumber } = require('ethers');
+const { ethers } = require('hardhat');
 
-describe("LinearVesting contract", function () {
+describe('LinearVesting contract', function () {
   let linearVesting;
   let liteCoin;
-  const time  = 10**3 * 60;  // 1 second
+  let scheduleID;
+  const time  = 10**3 * 60;  // 60 second
 
   before(async function () {
     linearVesting = await ethers.getContractFactory('LinearVesting');
@@ -17,39 +18,63 @@ describe("LinearVesting contract", function () {
     linearVesting = await linearVesting.deploy();
     await linearVesting.deployed();
   });
+  
+  describe('Check Mint function', function () {
+    describe ('Call Mint function', function () {
+      it ('Calling mint(tokenAddr, toAddr, time) should be succeed.', async function() {
+        const [owner, addr1] = await ethers.getSigners();  
+        const ownerBalance = await liteCoin.balanceOf(owner.address);
+        await liteCoin.increaseAllowance(linearVesting.address, ownerBalance);
+        await linearVesting.mint(liteCoin.address, addr1.address, time);
+      });
+  
+      it ('If owner does not have token balance, mint function should be failed', async function() {
+        let isError = false;
+        try {
+          await linearVesting.mint(linteCoin.address, addr1.address, time);
+        } catch(e) {
+          isError = true;
+        }
+        assert.equal(isError, true);
+      });
+    });
 
-  beforeEach(async function () {});
+    describe ('After mint, param checking', function () {
+      it ('Last ScheduleID should be zero', async function () {
+        scheduleID = await linearVesting.getLastScheduleID();
+        assert.equal(scheduleID, 0);
+      });
 
-  const setAllowance = async (owner) => {
-    const ownerBalance = await liteCoin.balanceOf(owner.address);
-    await liteCoin.increaseAllowance(linearVesting.address, ownerBalance);
-  }
+      it ('Duration should be 60 seconds', async function () {
+        assert.equal(await linearVesting.duration(scheduleID), time);
+      });
 
-  // Test case
-  it('Check mint function', async function () {
-    const [owner, addr1] = await ethers.getSigners();
-    await setAllowance(owner);
-    await linearVesting.mint(liteCoin.address, addr1.address, time);
+      it ('Beneficiary address should be second owner address', async function () {
+        const [owner, addr1] = await ethers.getSigners();    
+        assert.equal(await linearVesting.beneficiary(scheduleID), addr1.address);
+      });
+
+      it ('First owner address should be has zero balance.', async function () {
+        const [owner, addr1] = await ethers.getSigners();    
+        assert.equal(await liteCoin.balanceOf(owner.address), 0);
+      });
+
+      it ('Beneficiary address should be has first Owner origin balance.', async function () {
+        const [owner, addr1] = await ethers.getSigners();    
+        const toBalance = await liteCoin.balanceOf(addr1.address);
+        const ownerBalance = await liteCoin.balanceOf(owner.address);
+        assert.equal(toBalance.eq(ethers.BigNumber.from(ownerBalance)), true);
+      });
+    });
   });
 
-  it('After mint, param checking', async function () {
-    const [owner, addr1] = await ethers.getSigners();
-    const scheduleID = await linearVesting.getLastScheduleID();
-
-    assert.equal(scheduleID, 0);
-    assert.equal(await linearVesting.duration(scheduleID), time);
-    assert.equal(await linearVesting.beneficiary(scheduleID), addr1.address);
-    assert.equal(await liteCoin.balanceOf(owner.address), 0);
-    const toBalance = await liteCoin.balanceOf(addr1.address);
-    const ownerBalance = await liteCoin.balanceOf(owner.address);
-    assert.equal(toBalance.eq(ethers.BigNumber.from(ownerBalance)), true);
-  });
-
-  it('Check redeem', async function () {
-    const [owner, addr1] = await ethers.getSigners();
-    const scheduleID = await linearVesting.getLastScheduleID();
-    await linearVesting.redeem(scheduleID);
-    console.log(await liteCoin.balanceOf(addr1.address));
+  describe ('Check redeem function', function () {
+    it ('Redeem amount should be bigger than zero.', async function() {
+      const [owner, addr1] = await ethers.getSigners();    
+      await linearVesting.redeem(scheduleID);
+      const balance = await liteCoin.balanceOf(addr1.address);
+      assert.equal(balance > 0, true);
+    })
   });
 
 });
